13d3237de17a2311da2f957286e9ee3ead9579a8
Saaral_Note <khosoo.fsdev@gmail.com>
2024-01-16 12:08:33 +0800

    efsrgd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import { Button, TextField, Card } from "@/components";
-import Link from "next/link";
-import styles from "@/styles/pages/Home.module.css";
+// import { Button, TextField, Card } from "@/components";
+// import Link from "next/link";
+// import styles from "@/styles/pages/Home.module.css";
 import Tailwind from "./tailwindcss";
 
 export default function Home() {
