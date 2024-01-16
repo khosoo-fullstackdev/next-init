e0176dbce6c1f6e819f78fb493e890e11d95c8c4
Saaral_Note <khosoo.fsdev@gmail.com>
2024-01-16 10:29:04 +0800

    i

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 import { Button, TextField, Card } from "@/components";
 import Link from "next/link";
 import styles from "@/styles/pages/Home.module.css";
+import Tailwind from "./tailwindcss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Home</h1>
-      <Link href={"/abc"}>Move To Abc</Link>
-      <Button />
-      <TextField />
-      <Card text="card text" />
+    <div>
+      <Tailwind />
     </div>
   );
 }
